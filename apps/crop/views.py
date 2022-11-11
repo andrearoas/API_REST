8bf1ba31cf2f146232d3d0262ecf91691b697d88
Paola Roa <paola.roa@correounivalle.edu.co>
2022-11-11 00:19:59 -0500

    2do commit

--- apps/crop/views.py
+++ apps/crop/views.py
@@ -1,26 +1,26 @@
-from rest_framework import viewsets, permissions
-# from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from apps.crop.models import Crop
 from apps.crop.serializers import CropSerializer
 
 
-class CropViewSet(viewsets.ModelViewSet):
+# class CropViewSet(viewsets.ModelViewSet):
+#     queryset = Crop.objects.all()
+#     permission_classes = [permissions.AllowAny]
+#     serializer_class = CropSerializer
+
+class CropCreate(generics.CreateAPIView):
     queryset = Crop.objects.all()
-    permission_classes = [permissions.AllowAny]
+    serializer_class = CropSerializer
+    permission_classes = (AllowAny,)
+
+
+class CropList(generics.ListAPIView):
+    queryset = Crop.objects.all()
     serializer_class = CropSerializer
 
-# class CropCreate(generics.CreateAPIView):
-#     queryset = Crop.objects.all()
-#     serializer_class = CropSerializer
-#     permission_classes = (AllowAny,)
-#
-#
-# class CropList(generics.ListAPIView):
-#     queryset = Crop.objects.all()
-#     serializer_class = CropSerializer
-#
-#
-# class CropDestroyUpdate(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Crop.objects.all()
-#     permission_classes = (IsAuthenticated,)
-#     serializer_class = CropSerializer
+
+class CropDestroyUpdate(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Crop.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = CropSerializer
--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -11,28 +11,28 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = (
             'id',
-            'name_user',
-            'lastname_user',
-            'email_user',
+            'first_name',
+            'last_name',
+            'email',
         )
 
 
 class UserLoginSerializer(serializers.ModelSerializer):
     token = serializers.CharField(allow_blank=True, read_only=True)
-    email_user = serializers.EmailField()
+    email = serializers.EmailField()
 
     class Meta:
         model = User
-        fields = ('token', 'email_user', 'password_user')
+        fields = ('token', 'email', 'password')
         extra_kwargs = {
-            'password_user': {
+            'password': {
                 'write_only': True
             },
         }
 
     def validate(self, data):
-        user = get_object_or_404(User, email_user=data['email_user'])
-        if not user.check_password(raw_password=data['password_user']):
+        user = get_object_or_404(User, email=data['email'])
+        if not user.check_password(raw_password=data['password']):
             raise ValidationError("Contraseña incorrecta", status.HTTP_400_BAD_REQUEST)
         token, _ = Token.objects.get_or_create(user=user)
         data['token'] = token
@@ -40,28 +40,28 @@ class UserLoginSerializer(serializers.ModelSerializer):
 
 
 class SignupSerializer(serializers.ModelSerializer):
-    password_user = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ('email_user',
-                  'name_user',
-                  'lastname_user',
-                  'password_user',
+        fields = ('email',
+                  'first_name',
+                  'last_name',
+                  'password',
                   'password2')
 
-        extra_kwargs = {"password_user": {'write_only': True}}
+        extra_kwargs = {"password": {'write_only': True}}
 
     def create(self, validated_data):
         return User.objects.create_user(
-            email_user=validated_data['email_user'],
-            name_user=validated_data['name_user'],
-            lastname_user=validated_data['lastname_user'],
-            password_user=validated_data['password_user']
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password']
         )
 
     def validate(self, attrs):
-        if attrs['password_user'] != attrs['password2']:
-            raise serializers.ValidationError({"password_user": "Passwords doesn't match."})
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Passwords doesn't match."})
         return attrs
